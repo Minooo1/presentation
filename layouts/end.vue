27bbe7b9114f5da84be39c051fac90d4e9752858
Minooo1 <meiyeyoutai@gmail.com>
2023-10-31 01:24:05 +0900

    edit: layout end

---
+++ layouts/end.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1 class="end-title">ご静聴ありがとうございました</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  place-items: center;
+}
+
+.end-title {
+  font-size: 3.2vw;
+}
+</style>
