72389667f3306df7c741890ffb3be3b1489967ff
Minooo1 <meiyeyoutai@gmail.com>
2023-11-23 19:30:47 +0900

    feat: basic auth

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 import protect from 'static-auth';
 import safeCompare from 'safe-compare';
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const USER_NAME = process.env.USER_NAME || "admin";
 const PASSWORD = process.env.PASSWORD || "admin";
@@ -13,7 +8,7 @@ const app = protect(
   '/',
   (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD),
   {
-    directory: `${__dirname}/dist`,
+    directory: "/dist",
     onAuthFailed: (res) => {
       res.end('Authentication failed')
     },
