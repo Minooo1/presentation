c812132dcfeffdaa7ce605d08ae9d6cef415d356
Minooo1 <meiyeyoutai@gmail.com>
2023-11-23 19:19:32 +0900

    feat: basic auth

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const protect = require('static-auth');
-const safeCompare = require('safe-compare');
+import protect from 'static-auth';
+import safeCompare from 'safe-compare';
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-const USER_NAME = 'user';
-const PASSWORD = 'password';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const USER_NAME = process.env.USER_NAME || 'admin'; // プロジェクトの環境変数を設定していた場合はそちらを適用させる
+const PASSWORD = process.env.PASSWORD || 'admin';
 
 const app = protect(
   '/',
-  (username, password) => {
-    return safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD)
-  },
+  (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD), // timing attack 対策
   {
-    directory: `${ __dirname }/dist`,
+    directory: `${__dirname}/dist`, // public 配下のファイルを静的コンテンツとして配信する
     onAuthFailed: (res) => {
-      res.end('Authentication failed.')
+      res.end('Authentication failed')
     },
   }
-);
+)
 
-module.exports = app;
+export default app;
