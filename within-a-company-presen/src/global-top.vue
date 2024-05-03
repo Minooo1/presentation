b2cf2209acb82b380c7baba2da64a135998903ae
Minooo1 <meiyeyoutai@gmail.com>
2024-05-03 16:49:58 +0900

    refacotr: プレゼン資料

--- within-a-company-presen/src/global-top.vue
+++ within-a-company-presen/src/global-top.vue
@@ -1,7 +1,13 @@
 <template>
   <footer class="absolute top-0 left-3">
-    <Titles v-if="hasTitle" :no="//@ts-ignore
-    $slidev.nav.currentPage" class="font-bold border-solid border-b-4 border-b-[#56ffff] pb-2" />
+    <Titles
+      v-if="hasTitle"
+      :no="
+        //@ts-ignore
+        $slidev.nav.currentPage
+      "
+      class="font-bold border-solid border-b-4 border-b-[#56ffff] pb-2"
+    />
   </footer>
 </template>
 
@@ -12,19 +18,21 @@ import { computed } from "vue";
 
 type TypeTree = {
   active: boolean;
-  children: string[]
+  children: string[];
   hasActiveParent: boolean;
   hideInToc: boolean;
   level: number;
   path: string;
   title: string;
-}
+};
 
 const hasTitle = computed(() => {
   //@ts-ignore
-  const trees: TypeTree[] = $slidev.nav.tree;
+  const trees: TypeTree[] = $slidev.nav.tocTree;
 
-  //@ts-ignore
-  return trees.find(tree => tree.path === $slidev.nav.currentPath.replace("/", ""));
-})
+  return trees.find(
+    //@ts-ignore
+    (tree) => tree.path === $slidev.nav.currentPath.replace("/", "")
+  );
+});
 </script>
