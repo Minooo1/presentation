c1c6146eee8442011ff929595803ba86f8600b25
Minooo1 <meiyeyoutai@gmail.com>
2024-05-05 19:33:45 +0900

    update: config vite LT資料

---
+++ lt-kamakura-go/src/vite.config.ts
@@ -0,0 +1,81 @@
+import { defineConfig, HtmlTagDescriptor } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+const title = "kamakura.Go #6 LT発表スライド";
+const imagePath = "/images/gopher.png";
+const description = "kamakura.Goで初めてのLTで登壇した時のスライドです。";
+
+const metaDatas = [
+  // OG
+  {
+    property: "og:type",
+    content: "website",
+  },
+  {
+    property: "og:image",
+    content: imagePath,
+  },
+  {
+    property: "og:title",
+    content: title,
+  },
+  {
+    property: "og:description",
+    content: description,
+  },
+  {
+    property: "og:locale",
+    content: "ja_JP",
+  },
+  {
+    property: "og:site_name",
+    content: title,
+  },
+  {
+    property: "og:image:width",
+    content: "1200",
+  },
+  {
+    property: "og:image:height",
+    content: "600",
+  },
+  // Twitter
+  {
+    property: "twitter:card",
+    content: "summary",
+  },
+  {
+    property: "twitter:title",
+    content: title,
+  },
+  {
+    property: "twitter:description",
+    content: description,
+  },
+  {
+    property: "twitter:image",
+    content: imagePath,
+  },
+  // ETC
+  {
+    property: "keywords",
+    content: "プレゼン, スライド, Slidev, Go",
+  },
+];
+
+const generateMetaTag = (meta: typeof metaDatas[number]): HtmlTagDescriptor => ({
+  injectTo: "head-prepend",
+  tag: "meta",
+  attrs: { ...meta },
+});
+
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        tags: metaDatas.map(generateMetaTag),
+      },
+    }),
+  ],
+})
